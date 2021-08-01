86d82bf9a3f9d79b6f594603e58ef6ea09b78ee3
Anna Moelling <anna.moelling@googlemail.com>
2021-08-01 13:09:13 +0200

    updated version of guard

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'creating articles' do
     fill_in "Title", with: "Creating a blog"
     fill_in "Body", with: "Lorem Ipsum"
     click_button "Create Article"
-    
+  
     expect(page).to have_content("Article has been created.")
     expect(page.current_path).to eq(articles_path)
   end
