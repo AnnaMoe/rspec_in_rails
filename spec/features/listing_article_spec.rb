49e394d8d84cac3eb89254cb82f97cb280cfd1fe
Anna Moelling <anna.moelling@googlemail.com>
2021-08-02 16:20:02 +0200

    testing for no found article

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Listing articles' do
   end
 
   scenario 'a user lists all articles' do
-    visit "/"
+    visit "/" 
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
     expect(page).to have_content(@article2.title)
@@ -15,5 +15,22 @@ RSpec.describe 'Listing articles' do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
+  scenario 'a user has no articles' do
+    Article.delete_all
+    visit "/"
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within("h1#no-articles") do
+      expect(page).to have_content("No Articles created")
+    end
+  
+  end
+
   
 end
