187dd4bdd6a9549111acf28815ff4400b27a301d
Anna Moelling <anna.moelling@googlemail.com>
2021-08-08 17:52:00 +0200

    show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Listing articles' do
     @article2 = Article.create(title: "Second article", body: "Body of second article", user: @john) 
   end
 
-  scenario 'a user lists all articles' do
+  scenario 'with articles created and user not signed in' do
     visit "/" 
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
@@ -15,6 +15,19 @@ RSpec.describe 'Listing articles' do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario 'a user lists all articles' do
+    login_as(@john)
+    visit "/" 
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
   end
 
   scenario 'a user has no articles' do
