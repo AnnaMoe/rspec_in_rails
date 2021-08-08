56b5667b0dabb6d507f1471eaa1ef37d9d43f37e
Anna Moelling <anna.moelling@googlemail.com>
2021-08-08 17:11:40 +0200

    fixed broken show

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'show an article' do
   before do
-    @article = Article.create(title: "First article", body: "Lorem ipsolum") 
+    @john = User.create(email: "john@example.com", password: "password")
+    #this method is provided by warden - need to include Warden in rails-helper
+    login_as(@john)
+    @article = Article.create(title: "First article", body: "Lorem ipsolum", user: @john) 
   end
 
   it 'a user can see the details of one article' do
